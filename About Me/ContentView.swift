054e8c070794bfc64108ce97c2250ad734acdf89
Shruti Shivakumar <shivakumarshruti1@gmail.com>
2025-07-24 09:41:21 -0400

    In this time, I added a title and a text to the VStack in contentView, and I added a ZStack with a RadialGradient around it as the background.

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Shruti Shivakumar")
+        // make sure to change the colors to lighter versions when I figure out how to
+        
+        ZStack {
+            RadialGradient(colors: [Color.green, Color.pink], center: .topLeading, startRadius: 999, endRadius: 111)
             
+            
+            
+            VStack {
+                Text("Shruti Shivakumar")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.black)
+                Text("Shruti is 16 years old and an incoming high school junior. She loves art, business, computer science, math, community service, and music. She is also a passionate feminist. She aims to become a CEO of a company.")
+                    .font(.title3)
+            }
         }
+        
+        
         .padding()
+        
+        
+        
+        
+        
+        
     }
 }
 
